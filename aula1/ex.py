9f3bbef2450ba33f1247b87d4debf8a5f472d5d8
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2018-10-23 23:07:29 -0200

    aula1

---
+++ aula1/ex.py
@@ -0,0 +1,9 @@
+idade = int(input('qual a sua idade? '))
+if (idade>=18):
+    print("voce pode entrar no cinema")
+else:
+    bin = str(input('Esta acompanhado dos pais?'))
+    if(bin=='sim'):
+        print("voce pode entrar no cinema")
+    else:
+        print("voce nao pode entrar no cinema")
---
+++ aula1/hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+mensagem = 'ola seja bem vindo'
+nome = input('Digite seu nome ')
+print(mensagem, nome)
+
+print(mensagem, nome, sep=': ', end='\n\n\n\n')
+
+#end= '\n' pula a linha, sep= string troca o separador da variavel
+
